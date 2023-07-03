024c8ca0b3e3ba40abffa4aae3d2f8f1edf8a596
afzal-khan-in <khanafzal196.com>
2023-07-04 00:01:08 +0530

    Episode-03

--- App.js
+++ App.js
@@ -1,35 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-/**
- *
- * <div id="parent">
- * <div id="child">
- * <h1>I'm a h1 tag</h1>
- * <h2>I'm a h2 tag</h2>
- * </div>
- * <div id="child2">
- * <h1>I'm a h1 tag</h1>
- * <h2>I'm a h2 tag</h2>
- * </div>
- * </div>
- *
- *
- */
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I'm a h1 tag"),
-    React.createElement("h2", {}, "I'm a h2 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I'm a h1 tag"),
-    React.createElement("h2", {}, "I'm a h2 tag"),
-  ]),
-]);
-// const heading = React.createElement(
-//   "h1",
-//   { id: "heading" },
-//   "Hello world from react"
-// );
+//JSX
+
+const jsxHeading = <h1>Namste React using JSX</h1>;
+
+// React component
+// Class basd component - old
+// Functional component - new
+const Title = () => <h1>Hi, i'm a H1 tag</h1>;
+// React functional component
+const HeadingComponent = () => (
+  <div id="container">
+    <Title />
+    <h1 className="heading">Namaste React functional component</h1>
+  </div>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<HeadingComponent />);
