24da52d0978f8e89ecba0e318767cd2939a59cc8
afzal-khan-in <khanafzal196.com>
2023-09-18 22:49:12 +0530

    Episode_6_Exploring_the_World_part_01

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,21 +1,40 @@
-import { useState } from "react";
-import listOfRestaurants from "../utils/RestaurantList";
+import { useState, useEffect } from "react";
+//import listOfRestaurants from "../utils/RestaurantList";
 import RestaurantCard from "./RestaurantCard";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
-  const [listOfRestaurants1, setlistOfRestaurants1] =
-    useState(listOfRestaurants);
+  const [listOfRestaurants, setlistOfRestaurants] = useState([]);
 
-  return (
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.3667195&lng=78.4285084&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    console.log(json);
+    setlistOfRestaurants(
+      json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
+
+  return listOfRestaurants.length === 0 ? (
+    <div className="body-container">
+      <Shimmer />{" "}
+    </div>
+  ) : (
     <div className="body-container">
       <div className="filter">
         <button
           type="button"
           onClick={() => {
-            const filterRestaurants = listOfRestaurants1.filter(
-              (res) => res.info.avgRating > 4
+            const filterRestaurants = listOfRestaurants.filter(
+              (res) => res.info.avgRating >= 4
             );
-            setlistOfRestaurants1(filterRestaurants);
+            setlistOfRestaurants(filterRestaurants);
             console.log(filterRestaurants);
           }}
         >
@@ -23,7 +42,7 @@ const Body = () => {
         </button>
       </div>
       <div className="res-container">
-        {listOfRestaurants1.map((items) => (
+        {listOfRestaurants.map((items) => (
           <RestaurantCard key={items.info.id} resData={items} />
         ))}
       </div>
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,3 @@
+// whenever we need to export named export then while importing we need to add curly braces
 export const CDN_URL =
   "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
